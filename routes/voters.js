43d0ef9ce85c1fa90d32b3ed4e70d2cfc0d6aa4e
DiawuoDI <diawuodivine.dd@gmail.com>
2023-07-02 13:25:18 +0000

    new y

--- routes/voters.js
+++ routes/voters.js
@@ -1,21 +1,19 @@
-
 const { Router } = require("express");
 const voterRouter = Router();
 const voter = require("../controllers/voters");
 const validation = require("../validation/voters");
-const verification = require("../verification/verify")
+const verification = require("../verification/verify");
 
+voterRouter.post("/", validation.checkVoterExists, voter.createVoter);
 
-voterRouter.post("/",validation.checkVoterExists, voter.createVoter);
+voterRouter.get("/login/", voter.login);
 
-voterRouter.get("/login/",voter.login);
+voterRouter.get("/", verification.verifyToken, voter.getAllVoters);
 
-voterRouter.get("/",verification.verifyToken,voter.getAllVoters);
+voterRouter.get("/:studentId", verification.verifyToken, voter.getVotersById);
 
-voterRouter.get("/:studentId", voter.getVotersById);
+voterRouter.delete("/", verification.verifyToken, voter.deleteVoter);
 
-voterRouter.delete("/", voter.deleteVoter);
+voterRouter.patch("/", verification.verifyToken, voter.updateVoter);
 
-voterRouter.patch('/',voter.updateVoter);
-
-module.exports = voterRouter
+module.exports = voterRouter;
