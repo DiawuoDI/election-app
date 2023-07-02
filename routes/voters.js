41969d91c6f7ed61272a6ff0555579c6f5018790
DiawuoDI <diawuodivine.dd@gmail.com>
2023-07-02 13:19:42 +0000

    new g

--- routes/voters.js
+++ routes/voters.js
@@ -3,13 +3,14 @@ const { Router } = require("express");
 const voterRouter = Router();
 const voter = require("../controllers/voters");
 const validation = require("../validation/voters");
+const verification = require("../verification/verify")
 
 
 voterRouter.post("/",validation.checkVoterExists, voter.createVoter);
 
 voterRouter.get("/login/",voter.login);
 
-voterRouter.get("/",voter.getAllVoters);
+voterRouter.get("/",verification.verifyToken,voter.getAllVoters);
 
 voterRouter.get("/:studentId", voter.getVotersById);
 
